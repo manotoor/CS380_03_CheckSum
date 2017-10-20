73e7d310f2e35953b9ec519f2f406d004d0fd63d
manotoor <manotoor@gmail.com>
2017-10-19 21:19:16 -0700

    Working Checksum but doesn't work with odds

--- Ex3Client.java
+++ Ex3Client.java
@@ -13,20 +13,28 @@ public class Ex3Client{
 
 			//Read in int value of how big the array is
 			int size = is.read();
+			System.out.println("Reading " + size + " bytes.");
 			byte[] values = new byte[size];
 			//Read in array and print to screen
+			System.out.print("Bytes Read: ");
 			for(int i =0; i < size;i++){
 				values[i] = (byte)is.read();
 				if((i%10)==0)
-					System.out.print("\n");
+					System.out.print("\n\t");
 				System.out.printf("%02X",values[i]);
 			}
 			//Checksum calculations
 			short checkSum = checksum(values);
+			System.out.printf("\nChecksum calculated: 0x%02X\n",checkSum);
 			//Send Checksum back to server as 2 bytes
-
+			os.write(checkSum >> 8);
+			os.write(checkSum >> 0);
 			//Listen for response, if response 1 then good if 0 then bad
-			
+			if(is.read() == 1){
+				System.out.println("Response is Good.");
+			}else{
+				System.out.println("Response is Bad.");
+			}
 		}catch(Exception e){
 			System.out.println("\nSomething went wrong with server\n" + e);
 		}
@@ -35,14 +43,25 @@ public class Ex3Client{
 	private static short checksum(byte[] b){
 		//Initialize Sum
 		int sum = 0;
+		//int i = 0;
 		//Loop through bytes
-			//while looping i/2
-			//take upper shift logical left 4 bits
-			//take next byte and add to uppper
-			//put back in result
+		for(int i =0; i < b.length-1; i= i+2){
+			//take upper shift 4 bits AND wit
+			byte upper = b[i];
+			byte lower = b[i+1];
 			//add to sum
+			sum = sum + ((upper << 8 & 0xFF00) + (lower & 0xFF));
 			//check to make sure no overflow
+			if ((sum & 0xFFFF0000) > 0) {
+				sum &= 0xFFFF;
+				sum++;
+			}
+		}
+		//For Odd
+		if(b.length %2 == 1){
+			//add odd bit to sum
+		}
 		//return sum
-		return (short) ~ (sum & 0xFFFF);
+		return (short)~(sum & 0xFFFF);
 	}
 }
